65b8d6701b8fd9f55c1b0d04b216de1e161b5524
AbbasCherri <abbasbcherri@gmail.com>
2026-05-18 23:27:20 +0300

    Initialize CRN frontend

---
+++ reggie/crn/crn_frontend.py
@@ -0,0 +1,9 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel
+
+app = QApplication([])
+label = QLabel("<font color=red size=100> I AM THE PYQT GOD MWAHAHAH</font>")
+label.show()
+app.exec()
